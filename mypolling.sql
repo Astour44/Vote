eace129ec86b0af8885d46823534019340bad6d1
Astour44 <astour44@gmail.com>
2021-12-24 08:15:36 +0800

    完成

---
+++ mypolling.sql
@@ -0,0 +1,162 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2021-12-24 01:14:45
+-- 伺服器版本： 10.4.22-MariaDB
+-- PHP 版本： 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫: `mypolling`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `ad`
+--
+
+CREATE TABLE `ad` (
+  `id` int(11) NOT NULL,
+  `name` int(11) NOT NULL,
+  `sh` int(11) NOT NULL,
+  `intro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `options`
+--
+
+CREATE TABLE `options` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `opt` varchar(128) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `count` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `topic_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 傾印資料表的資料 `options`
+--
+
+INSERT INTO `options` (`id`, `opt`, `count`, `topic_id`) VALUES
+(1, '忘憂亭', 6, 5),
+(2, 'D Park台肥海洋深層水園區', 1, 5),
+(3, '海崖谷', 0, 5),
+(4, '翡翠谷', 2, 5),
+(9, '陽明山', 72, 1),
+(10, '白沙灣', 9, 1),
+(11, '不厭亭', 5, 1),
+(13, '嘉義阿里山日出', 5, 3),
+(17, '藍晒圖文創園區', 0, 3),
+(21, '石岡落羽松森林秘境', 8, 2),
+(22, '台中水景公園', 3, 2),
+(24, '朝馬秋紅谷', 0, 2),
+(25, '台中龍井', 0, 2);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `topics`
+--
+
+CREATE TABLE `topics` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `topic` varchar(128) COLLATE utf8mb4_unicode_520_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 傾印資料表的資料 `topics`
+--
+
+INSERT INTO `topics` (`id`, `topic`) VALUES
+(1, '北部區域'),
+(2, '中部區域'),
+(3, '南部區域\r\n'),
+(5, '東部區域');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `account` varchar(12) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `password` varchar(12) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `email` varchar(24) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `name` varchar(12) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `gender` varchar(1) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `birthday` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 傾印資料表的資料 `users`
+--
+
+INSERT INTO `users` (`id`, `account`, `password`, `email`, `name`, `gender`, `birthday`) VALUES
+(1, 'mack', '1234', 'macklun@ms7.hinet.net', '劉勤永', '男', '1974-10-07'),
+(2, 'pp', 'pp', 'pp', 'pp', 'p', '2021-12-02'),
+(3, 'aa', 'aa', 'aa', 'aa', 'a', '2021-12-01');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `topics`
+--
+ALTER TABLE `topics`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `options`
+--
+ALTER TABLE `options`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `topics`
+--
+ALTER TABLE `topics`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
